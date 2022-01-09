79f7dbbe63e7ae059cbf890393a59a3bc7545a45
Bhargavi Mahashabde <60249812+Bhargavimahashabde@users.noreply.github.com>
2022-01-09 12:32:35 +0530

    Update Q.6 greatest common divisor of strings.cpp

--- Goldman Sachs/Q.6 greatest common divisor of strings.cpp
+++ Goldman Sachs/Q.6 greatest common divisor of strings.cpp
@@ -1,8 +1,5 @@
 Question:
-6. Given two strings str1 and str2. We say that str2 divides str1 if it's possible
-   to concatenate multiple str2 to get str1. For example, ab divides abab. 
-   if str2 does not divide str1, return -1. Otherwise, return the smallest string 
-   str3 such that str3 divides both str1 and str2.
+6.  greatest common divisor of strings
      
 Solution:
 #include <bits/stdc++.h>
