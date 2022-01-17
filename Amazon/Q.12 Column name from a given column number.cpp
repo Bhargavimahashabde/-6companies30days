1c04f4f541033c2cdb7522359a5b79d8d395cca1
Bhargavi Mahashabde <60249812+Bhargavimahashabde@users.noreply.github.com>
2022-01-17 12:29:47 +0530

    Create Q.12 Column name from a given column number.cpp

---
+++ Amazon/Q.12 Column name from a given column number.cpp
@@ -0,0 +1,32 @@
+Question:
+12. Column name from a given column number
+
+Solution:
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+    string colName(long long int n)
+    {
+        // your code here
+        string temp = "";
+        while (n > 0)
+        {
+            char ch = 'A' + (n - 1) % 26;
+            temp = ch + temp;
+            n = (n - 1) / 26;
+        }
+        return temp;
+    }
+
+
+int main()
+{
+
+    long long int n;
+    cin >> n;
+
+    cout << colName(n) << '\n';
+}
+
