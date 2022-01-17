518b6105d279a07b42f7b2d62a9193f88ef3ba45
Bhargavi Mahashabde <60249812+Bhargavimahashabde@users.noreply.github.com>
2022-01-17 12:15:52 +0530

    Create Q.8 Count ways to Nth Stair.cpp

---
+++ Amazon/Q.8 Count ways to Nth Stair.cpp
@@ -0,0 +1,26 @@
+Question:
+8. Count ways to Nth Stair
+
+Solution:
+#include <bits/stdc++.h>
+
+using namespace std;
+
+    long long countWays(int m)
+    {
+        // your code here
+        return (m/2)+1;
+    }
+
+
+
+int main()
+{
+
+        int m;
+        cin>>m;
+     
+        cout<<countWays(m)<<endl;
+    return 0;
+}
+
