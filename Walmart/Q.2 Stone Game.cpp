bf43e89d592664bfac1a70b35e37f0ddef0f9128
Bhargavi Mahashabde <60249812+Bhargavimahashabde@users.noreply.github.com>
2022-02-02 00:13:25 +0530

    Create Q.2 Stone Game.cpp

---
+++ Walmart/Q.2 Stone Game.cpp
@@ -0,0 +1,24 @@
+Question:
+2. Stone Game
+
+Solution:
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool stoneGame(vector<int> &piles)
+    {
+        return true;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> v = {1, 2, 3, 4, 5};
+    cout << s.stoneGame(v) << endl;
+    return 0;
+}
+
